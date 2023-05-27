c64911431aacc872d9b0b68d65fa5ab74ce33b82
kathychen55555 <129899421+kathychen55555@users.noreply.github.com>
2023-05-26 21:46:14 -0700

    Delete test-mapFunctions.R

--- R/test-mapFunctions.R
+++
@@ -1,6 +0,0 @@
-test_that("case1 getMapInfo", {
-  expect_error(getMapInfo(12), "Function must take in the map name in form of a string")
-  expect_error(getMapInfo("disneyland"), "Function must take in a map from the game VALORANT")
-})
-
-
