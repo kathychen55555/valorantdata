6cc51f879eb01806eb3b0ff9b19795892266f025
kathychen55555 <129899421+kathychen55555@users.noreply.github.com>
2023-05-26 21:30:25 -0700

    fixed a typo on maps fcn

---
+++ mapFunctions.R
@@ -0,0 +1,23 @@
+#' Gets map information about inputted map
+#'
+#' @param map name of map in a string
+#'
+#' @return A dataframe that lists all of the locations on the map
+#'
+#' @export
+getMapInfo <- function(map) {
+  if (class(map) != "character") {
+    stop("Function must take in the map name in form of a string")
+  }
+  else if (!(stringr::str_to_title(map) %in% map_id$map)) {
+    stop("Function must take in a map from the game VALORANT")
+  }
+  api <- httr::GET(paste("https://valorant-api.com/v1/maps/",
+                        map_id[map_id$map == stringr::str_to_title(map),]$uuid,
+                        sep = ""))
+  mapInfo <- jsonlite::fromJSON(rawToChar(api$content))$data$callouts
+
+  names(mapInfo) <- c("Name", "Location", "coordinate")
+
+  return(mapInfo)
+}
